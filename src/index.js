6c23e43959754194b6fddf0a1dcece915de2ec10
Paul Le Cam <paul@mainframe.com>
2017-08-21 16:38:09 +0100

    Export types from module rather than separate file

--- src/index.js
+++ src/index.js
@@ -2,3 +2,12 @@
 
 export { convertToObject, convertToJSON } from './converter'
 export { renderFromObject, Renderer } from './renderer'
+export type {
+  ElementChild,
+  ElementChildren,
+  ElementProps,
+  ConvertedChild,
+  ConvertedChildren,
+  ConvertedProps,
+  ConvertedElement,
+} from './types'
